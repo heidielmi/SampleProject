4681f85209b22f823efccb5ea53e41e87a4a9e5c
heidielmi <heidielmi@yahoo.com>
2018-01-18 10:32:34 +1100

    usinf reduce and map

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PetsService, IPetOwner, IPet, genderEnum } from '../shared/services/pet';
+import { Observable, BehaviorSubject } from 'rxjs/Rx';
+export type OwnersAndPets = { catsForFemales$: Observable<IPet[]>, catsForMales$: Observable<IPet[]> };
 @Component({
   selector: 'sample-dashboard-page',
   templateUrl: './dashboard-page.component.html'
@@ -11,61 +13,85 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   maleOwners: IPetOwner[] = [];
   catsForFemaleOwner: IPet[] = [];
   catsForMaleOwner: IPet[] = [];
-  _sub: any
+
+  petOwners_apparoach2: IPetOwner[] = [];
+  catsForFemaleOwner_apparoach2: IPet[] = [];
+  catsForMaleOwner_apparoach2: IPet[] = [];
+  _sub: any;
+  _sub_rxjs: any;
+
+  ownersAndPets$: BehaviorSubject<IPetOwner[]> = new BehaviorSubject<IPetOwner[]>([]);
   constructor(private _petsService: PetsService) { }
 
   ngOnInit() {
-    this._sub = this._petsService.getAllPetOwners().subscribe( petOwnersApi =>{
+    this._sub = this._petsService.getAllPetOwners().subscribe(petOwnersApi => {
       //get all thge pet-owners from web service
       this.petOwners = petOwnersApi;
 
       // filter female owners and their cats
-      this.femaleOwners= this.petOwners.filter(petowner => petowner.gender === genderEnum.female);
-      this.catsForFemaleOwner = this.populatePets(this.femaleOwners, "cat");
+      this.catsForFemaleOwner = this.populatePets(this.petOwners, "cat", genderEnum.female);
 
       // filter male owners and their cats
-      this.maleOwners= this.petOwners.filter(petowner => petowner.gender === genderEnum.male);
-      this.catsForMaleOwner = this.populatePets(this.maleOwners, "cat");
+      this.catsForMaleOwner = this.populatePets(this.petOwners, "cat", genderEnum.male);
+    });
+
+    //second approach using RXJS(reactive programming)
+    this._petsService.getOwners();
+    this._sub_rxjs = this._petsService.owners$.subscribe(owners => {
+      this.petOwners_apparoach2 = owners;
+      // filter female owners and their cats
+      this.catsForFemaleOwner_apparoach2 = this.populatePets(this.petOwners_apparoach2, "cat", genderEnum.female);
+
+      // filter male owners and their cats
+      this.catsForMaleOwner_apparoach2 = this.populatePets(this.petOwners_apparoach2, "cat", genderEnum.male);
+
+
+    });
+
+
+  }
+
+  /**
+  *   get list of pet-owners and returns all their "cat" pets
+  */
+  populatePets(owners: IPetOwner[], petType: string, gender: genderEnum): IPet[] {
+    let calculatedCats: IPet[] = [];
+
+    calculatedCats = owners.filter(petowner => petowner.gender === gender)
+      //map ===> it return array of pet[] 
+      .map(owner => owner.pets)
+      // now should faltten this array of array
+      .reduce((a, b) => a.concat(b), [])
+      .filter(pet => pet.type.toLowerCase() == petType);
+    this.sortListByName(calculatedCats, true);
+    return calculatedCats;
+  }
+
+  /**
+  *   sort list based on the pet's name
+  */
+  sortListByName(petList: IPet[], isAsc: boolean) {
+    petList.sort(function (a, b) {
+
+      var nameA = a.name;
+      var nameB = b.name;
+
+      if (nameA < nameB)
+        return (isAsc === true) ? -1 : 1;
+      if (nameA > nameB)
+        return (isAsc === true) ? 1 : -1;
+      return 0;  //no sorting
+
     });
   }
-    /**
-    *   get list of pet-owners and returns all their "cat" pets
-    */
-    populatePets(owners: IPetOwner[], petType: string): IPet[] {
-      let calculatedCats :IPet[] = [];
-      for (let owner of owners){
-        for (let pet of owner.pets) {
-          if(pet.type.toLowerCase() === petType){
-            calculatedCats.push(pet);
-          }
-         }
-      }
-      this.sortListByName(calculatedCats, true);
-     return calculatedCats;
+
+  ngOnDestroy() {
+    if (this._sub) {
+      this._sub.unsubscribe();
     }
-
-    /**
-    *   sort list based on the pet's name
-    */
-    sortListByName( petList : IPet[], isAsc: boolean){
-      petList.sort(function (a,b) {
-
-       var nameA = a.name;
-       var nameB = b.name;
-
-       if (nameA < nameB)
-         return (isAsc=== true) ? -1 : 1;
-       if (nameA > nameB)
-         return (isAsc=== true) ? 1 : -1;
-       return 0;  //no sorting
-
-       });
-     }
-
-     ngOnDestroy() {
-      if (this._sub) {
-          this._sub.unsubscribe();
-      }
+    if (this._sub_rxjs) {
+      this._sub_rxjs.unsubscribe();
+    }
   }
 
 }
--- src/app/shared/services/pet/pets.service.ts
+++ src/app/shared/services/pet/pets.service.ts
@@ -1,20 +1,20 @@
-import { Injectable }  from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import 'rxjs/Rx';
+import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs/Rx';
 
 export enum genderEnum {
-   male,
-   female,
-   other
+  male,
+  female,
+  other
 }
 
 export interface IPetOwner {
   name: string,
   gender: genderEnum,
   age: string,
-  pets:IPet[]
+  pets: IPet[]
 }
 export interface IPet {
   name: string,
@@ -28,56 +28,72 @@ export interface IPetService {
  */
 @Injectable()
 export class PetsService implements IPetService {
-
-    constructor(private http: HttpClient) {
-   }
+  private store: {
+    owners: IPetOwner[],
+};
+  private _owners: BehaviorSubject<Array<IPetOwner>> = new BehaviorSubject([]);
+  public readonly owners$: Observable<Array<IPetOwner>> = this._owners.asObservable();
+  constructor(private http: HttpClient) {
+    this.store = { owners: []};
+  }
 
   /**
+   * user an other approach with reactive programming(RXJS)
    * getAllPetOwners - get the all pet owners
    * @param  
    * @return return  Observable<IPetOwner[]>
    */
   public getAllPetOwners(): Observable<IPetOwner[]> {
-       return this.transformToPetOwnerArray(this.http.get('http://agl-developer-test.azurewebsites.net/people.json'));
+    return this.transformToPetOwnerArray(this.http.get('http://agl-developer-test.azurewebsites.net/people.json'));
+  }
+   /**
+   * use RXJS logic to get all petowners as a stream
+   * shared/models/people.json is what azure service returns
+   */
+  getOwners(){
+    this.transformToPetOwnerArray(this.http.get('http://agl-developer-test.azurewebsites.net/people.json')).subscribe( peoples$ => {
+         this.store.owners = peoples$;
+         this._owners.next(this.store.owners);
+    })
   }
 
-   /**
-   *   transform any[] into Observable<IPet[]>
-   * @return {Observable<IPetOwner[]>}  return Angular2 Observable IPetOwner[] object
-   */
+  /**
+  *   transform any[] into Observable<IPet[]>
+  * @return {Observable<IPetOwner[]>}  return Angular2 Observable IPetOwner[] object
+  */
   private mapPets(petsResponse: any[]): IPet[] {
-      return petsResponse.map( (pet) =>  {
-                return <IPet> {
-                   name: pet.name,
-                   type: pet.type
-                }
-       })
+    return petsResponse.map((pet) => {
+      return <IPet>{
+        name: pet.name,
+        type: pet.type
+      }
+    })
   }
   /**
    *   transform what webApi is returning(Observable<any[]) into Observable<IPetOwner[]>
    * @return {Observable<IPetOwner[]>}  return Angular2 Observable IPetOwner[] object
    */
   private transformToPetOwnerArray(petOwners$: Observable<any>): Observable<IPetOwner[]> {
-    return petOwners$ .map((response: any[]) => {
-     return response.map( (petOwner) =>  {
-      return <IPetOwner>{
-        name: petOwner.name,
-        gender: this.ParseGender(petOwner.gender),
-        age: petOwner.age,
-        pets: (petOwner.pets)? this.mapPets(petOwner.pets) :[]
-      }
-     });
+    return petOwners$.map((response: any[]) => {
+      return response.map((petOwner) => {
+        return <IPetOwner>{
+          name: petOwner.name,
+          gender: this.ParseGender(petOwner.gender),
+          age: petOwner.age,
+          pets: (petOwner.pets) ? this.mapPets(petOwner.pets) : []
+        }
+      });
     });
   }
 
-  public  ParseGender(value: string): genderEnum {
-    if(value.trim().toLowerCase() === 'male'){
-             return genderEnum.male;
+  public ParseGender(value: string): genderEnum {
+    if (value.trim().toLowerCase() === 'male') {
+      return genderEnum.male;
     }
-    else if(value.trim().toLowerCase() === 'female'){
+    else if (value.trim().toLowerCase() === 'female') {
       return genderEnum.female;
     }
     return genderEnum.other;
   }
-  
+
 }
